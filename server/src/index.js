017f78b6fb1e8d3083c6266af0d7e4aa71af754c
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-11-15 10:11:39 +0530

    01_project_init

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.get("/health",(req,res)=>{
+    res.send("OK")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`You application is running on http://localhost:${ process.env.PORT}`);
+});
