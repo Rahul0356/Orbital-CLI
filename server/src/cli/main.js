ef2833c8da1be6c702fd4daf15eceb9e66f1397d
Rahul Kumar <rahulkumar9199270356@gmail.com>
2025-11-23 17:40:33 +0530

    03_device_flow_1

---
+++ server/src/cli/main.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+import dotenv from "dotenv";
+import chalk from "chalk";
+import figlet from "figlet";
+
+
+
+import {Command} from "commander";
+import { login } from "./commands/auth/login.js";
+
+
+dotenv.config();
+
+async function main() {
+    // Display Banner
+    console.log(
+        chalk.cyan(
+            figlet.textSync("Orbital CLI", {
+                font: "Standard",
+                horizontalLayout: "default",
+                
+            })
+        )
+    );
+
+    console.log(chalk.red("A CLI based AI Tool\n"));
+    const program = new Command("orbitals");
+
+    program.version("0.0.1")
+    .description("Orbital CLI - A Cli Based AI Tool")
+    .addCommand(login)
+
+    // Default action shows help
+    program.action(() => {
+        program.help();
+    });
+
+  program.parse()
+}
+
+main().catch((err)=>{
+    console.log(chalk.red("Error running orbital CLI:"),err)
+    process.exit(1)
+})
--- server/src/lib/auth.js
+++ server/src/lib/auth.js
@@ -2,12 +2,20 @@ import { betterAuth } from "better-auth";
 import {prismaAdapter} from "better-auth/adapters/prisma"
 import prisma from "./db.js"
 
+import { deviceAuthorization } from "better-auth/plugins";
+
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+        provider: "postgresql", 
     }),
     basePath:"/api/auth",
     trustedOrigins:["http://localhost:3000"],
+    plugins: [ 
+    deviceAuthorization({ 
+      expiresIn: "30m", 
+      interval: "5s",   
+    }), 
+  ], 
     socialProviders: { 
     github: { 
       clientId: process.env.GITHUB_CLIENT_ID , 
